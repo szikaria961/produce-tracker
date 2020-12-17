6e31e6bfb690900590f257726a7f6fcc6b52d7c4
szikaria961 <szikaria961@gmail.com>
2020-12-17 11:51:16 -0500

    Add custom filtering/fuzzy search for Name input field

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,159 @@
 const API_URL = 'api/produce';
+const MINIMUM_PRODUCE_NAME_LENGTH = 2;
+
 const produceNameElement = document.getElementById('produce-name');
 const numDaysElement = document.getElementById('num-days');
 const addProduceBtn = document.getElementById('add-produce-btn');
+const suggestionsRowElement = document.getElementById('suggestions-row');
+
+const produceSuggestionList = [
+  "alfalfa sprouts",
+  "apple",
+  "apricot",
+  "artichoke",
+  "asian pear",
+  "asparagus",
+  "atemoya",
+  "avocado",
+  "bamboo shoots",
+  "banana",
+  "beans",
+  "bean sprouts",
+  "beets",
+  "belgian endive",
+  "bitter melon",
+  "bell peppers",
+  "blackberries",
+  "blueberries",
+  "bok choy",
+  "boniato",
+  "boysenberries",
+  "broccoflower",
+  "broccoli",
+  "brussels sprouts",
+  "cabbage (green and red)",
+  "cantaloupe",
+  "carambola (star fruit or star apple)",
+  "carrots",
+  "casaba melon",
+  "cauliflower",
+  "celery",
+  "chayote",
+  "cherimoya (custard apple)",
+  "cherries",
+  "coconuts",
+  "collard greens",
+  "corn",
+  "cranberries",
+  "cucumber",
+  "dates",
+  "dried plums (a.k.a. prunes)",
+  "eggplant",
+  "endive",
+  "escarole",
+  "feijoa",
+  "fennel",
+  "figs (dry and fresh)",
+  "garlic",
+  "gooseberries",
+  "grapefruit",
+  "grapes",
+  "green beans",
+  "green onions",
+  "greens (turnip, beet, collard, mustard)",
+  "guava",
+  "hominy",
+  "honeydew melon",
+  "horned melon",
+  "iceberg lettuce",
+  "jerusalem artichoke",
+  "jicama",
+  "kale",
+  "kiwifruit",
+  "kohlrabi",
+  "kumquat",
+  "leeks",
+  "lemons",
+  "lettuce (boston, iceberg, leaf, romaine)",
+  "lima beans",
+  "limes",
+  "longan",
+  "loquat",
+  "lychee",
+  "madarins",
+  "malanga",
+  "mandarin oranges",
+  "mangos",
+  "mulberries",
+  "mushrooms",
+  "napa (chinese cabbage)",
+  "nectarines",
+  "okra",
+  "onion (green, red, spanish, yellow, white)",
+  "oranges",
+  "papayas",
+  "parsnip",
+  "passion fruit",
+  "peaches",
+  "pears",
+  "peas (green, snow, sugar snap)",
+  "peppers (bell – red, yellow, green, chili)",
+  "persimmons",
+  "pineapple",
+  "plantains",
+  "plums",
+  "pomegranate",
+  "potatoes",
+  "prickly pear (cactus pear)",
+  "prunes",
+  "pummelo (chinese grapefruit)",
+  "pumpkin",
+  "quince",
+  "radicchio",
+  "radishes",
+  "raisins",
+  "raspberries",
+  "red cabbage",
+  "rhubarb",
+  "romaine lettuce",
+  "rutabaga",
+  "shallots",
+  "snow peas",
+  "spinach",
+  "sprouts",
+  "squash (acorn, banana, buttercup, butternut,",
+  "summer)",
+  "strawberries",
+  "string beans",
+  "sweet potato",
+  "tangelo",
+  "tangerines",
+  "tomatillo",
+  "tomato",
+  "turnip",
+  "ugli fruit",
+  "watermelon",
+  "water chestnuts",
+  "watercress",
+  "waxed beans",
+  "yams",
+  "yellow squash",
+  "yuca/cassava",
+  "zucchini squash"
+];
+
+const commonProduceItems = [
+  "avocado",
+  "bananas",
+  "kale"
+];
+
+let selectedInput = "";
+
+let payload = {
+  name: '',
+  numDays: null
+}
 
 function loadAllProduce() {
   var xhttp = new XMLHttpRequest();
@@ -54,11 +206,6 @@ function handleDelete(id) {
   loadAllProduce();
 }
 
-let payload = {
-  name: '',
-  numDays: null
-}
-
 const handleSubmit = () => {
   const options = {
     method: 'post',
@@ -73,23 +220,71 @@ const handleSubmit = () => {
     .then(data => console.log(data));
 }
 
-produceNameElement.addEventListener('keyup', event => {
-  const updatedProduceName = event.target.value;
+function filterArray(pattern, inputArray) {
+  return inputArray.filter(item => item.includes(pattern));
+}
 
+function searchProduce(searchInput) {
+  return filterArray(searchInput, produceSuggestionList);
+}
+
+function showCommonItems() {
+  suggestionsRowElement.innerHTML = '';
+
+  commonProduceItems.forEach(item => {
+    const commonSuggestionElement = document.createElement('DIV');
+    commonSuggestionElement.innerHTML = `${item}`;
+    suggestionsRowElement.appendChild(commonSuggestionElement);
+  });
+}
+
+suggestionsRowElement.addEventListener('click', event => {
+  produceNameElement.value = event.target.innerHTML;
+});
+
+function showSuggestions(input) {
+  const filteredInputList = searchProduce(input);
+  suggestionsRowElement.innerHTML = '';
+
+  filteredInputList.forEach(item => {
+    const suggestionElement = document.createElement('DIV');
+    suggestionElement.innerHTML = `${item}`;
+    suggestionsRowElement.appendChild(suggestionElement);
+  });
+}
+
+function updateSuggestions() {
+  const inputString = produceNameElement.value;
+
+  if (inputString.length < MINIMUM_PRODUCE_NAME_LENGTH) {
+    showCommonItems();
+  } else {
+    showSuggestions(inputString);
+  }
+}
+
+function updatePayload(key, value) {
   payload = {
     ...payload,
-    name: updatedProduceName
+    [key]: value
   }
+}
+
+produceNameElement.addEventListener('keyup', event => {
+  updatePayload('name', event.target.value);
+  updateSuggestions();
 });
 
 numDaysElement.addEventListener('input', event => {
-  const updatedProduceNumberOfDays = event.target.value;
-
-  payload = {
-    ...payload,
-    numDays: updatedProduceNumberOfDays
-  }
+  updatePayload('numDays', event.target.value);
 });
 
 addProduceBtn.addEventListener('click', handleSubmit);
-loadAllProduce();
+
+
+function main() {
+  loadAllProduce();
+  updateSuggestions();
+}
+
+main();
